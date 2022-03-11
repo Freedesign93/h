3224a68c5d7ffc7bfac2f9e259a19a26729357c0
Freedesign93 <93400285+Freedesign93@users.noreply.github.com>
2022-03-12 02:28:10 +0700

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"622b9fa36ba4ec00186ae86a",
+				"622b9fa36ba4ec00186ae86d"
+			]
+		},
+		"622b9fa36ba4ec00186ae86a": {
+			"id": "622b9fa36ba4ec00186ae86a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"622b9fa36ba4ec00186ae86d": {
+			"id": "622b9fa36ba4ec00186ae86d",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
